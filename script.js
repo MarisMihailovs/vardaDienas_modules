7a9f357c4357b506aa96d1c4a80d57516771f548
MarisMihailovs <marismihailovs@gmail.com>
2022-12-23 17:26:51 +0200

    date added and mediaqueries of h elements adjusted

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ import { Dati } from "./vardadienas.js";
 
 const uzruna = document.querySelector('#uzruna');
 const vardi = document.querySelector('#vardi');
+const datums = document.querySelector('#datums')
 const d = new Date();
 const month = d.getMonth();
 const date = d.getDate();
+const year = d.getFullYear()
 
 console.log(d);
-console.log(month, date);
+console.log(date + "-" + month + "-" + year);
 console.log(Dati);
 
 vardi.innerText = Dati[month][date - 1] + "!";
+datums.innerText = date + "-" + month + "-" + year;
 
 
 
